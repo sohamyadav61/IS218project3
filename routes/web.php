f37bfdd534325801ee11bc54c916479df302bcf9
soham <sy357@njit.edu>
2019-05-16 16:50:37 -0400

    upload to heroku

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+
 
