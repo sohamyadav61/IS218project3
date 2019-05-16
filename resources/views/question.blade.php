f1e7b0632c6ec1cfcb423e1b8441668a2a3f5e1e
soham <sy357@njit.edu>
2019-05-16 19:28:05 -0400

    Completed the final part of the FAQ project which made sure that all the feature of questions and answers on the user side are working correctly

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -9,21 +9,26 @@
 
                     <div class="card-body">
 
+                        {{$question->body}}
                     </div>
                     <div class="card-footer">
                         <a class="btn btn-primary float-right"
-                           href="#">
+                           href="{{ route('questions.edit',['id'=> $question->id])}}">
                             Edit Question
                         </a>
+
+                        {{ Form::open(['method'  => 'DELETE', 'route' => ['questions.destroy', $question->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                        </button>
+                        {!! Form::close() !!}
                     </div>
                 </div>
             </div>
-        </div>
 
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header"><a class="btn btn-primary float-left"
-                                                href="#">
+                                                href="{{ route('answers.create', ['question_id'=> $question->id])}}">
                             Answer Question
                         </a></div>
 
